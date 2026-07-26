4509ce10691f3660f6c7d3b753681f8ab6716ef1
wesiwer <77644223+wesiwer@users.noreply.github.com>
2026-07-27 01:16:55 +0300

    Add calendar_screen.dart

---
+++ lib/features/calendar/calendar_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../../core/theme/app_theme.dart';
+
+class CalendarScreen extends StatelessWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.background,
+      appBar: AppBar(title: const Text('Календарь')),
+      body: const Center(child: Text('Интерактивный календарь - в разработке')),
+    );
+  }
+}
