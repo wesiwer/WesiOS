eb4b061ef689a94a674e91bbb4b398672a4ed328
wesiwer <77644223+wesiwer@users.noreply.github.com>
2026-07-28 00:40:15 +0300

    fix: correct AppTheme import path in glass_card.dart

--- lib/widgets/glass_card.dart
+++ lib/widgets/glass_card.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
+import '../core/theme/app_theme.dart';
 
 class GlassCard extends StatelessWidget {
   final Widget child;
