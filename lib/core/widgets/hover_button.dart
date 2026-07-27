60b8ed1a8a661f249901cda2dfd258ad7c9a7bbd
wesiwer <77644223+wesiwer@users.noreply.github.com>
2026-07-28 00:48:50 +0300

    fix: HoverButton.onTap type FutureOr<void> Function()? to support async callbacks + fix AppTheme import

--- lib/core/widgets/hover_button.dart
+++ lib/core/widgets/hover_button.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
+import '../core/theme/app_theme.dart';
 
 class HoverButton extends StatefulWidget {
   final Widget child;
-  final VoidCallback? onTap;
+  final FutureOr<void> Function()? onTap;
   final EdgeInsetsGeometry? padding;
   final BorderRadius? borderRadius;
   final Color? backgroundColor;
@@ -72,7 +73,7 @@ class _HoverButtonState extends State<HoverButton> {
 
 class HoverIconButton extends StatefulWidget {
   final IconData icon;
-  final VoidCallback? onTap;
+  final FutureOr<void> Function()? onTap;
   final double size;
   final Color? color;
 
