8f929508f22a29f69b5d3df9147be5c835fb01ee
wesiwer <77644223+wesiwer@users.noreply.github.com>
2026-08-02 23:16:31 +0300

    fix(knowledge): restore article editor main + part_a

--- lib/features/knowledge/screens/article_editor_screen.dart
+++ lib/features/knowledge/screens/article_editor_screen.dart
@@ -1,2 +1,16 @@
 import 'dart:convert';
-// RESTORE_MARKER_PLACEHOLDER_WILL_REPLACE
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import '../../../core/localization/wesi_locale.dart';
+import '../../../core/theme/app_theme.dart';
+import '../../../core/widgets/hover_button.dart';
+import '../../../core/widgets/window_controls.dart';
+import '../models/article_model.dart';
+import '../services/knowledge_service.dart';
+import 'special_chars.dart';
+import 'emoji_data.dart';
+import 'article_editor_charts.dart';
+import 'article_editor_media.dart';
+
+part 'article_editor_part_a.dart';
+part 'article_editor_part_b.dart';
